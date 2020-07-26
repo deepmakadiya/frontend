c18f78d16216a518a9bb4bb6156438404a454b66
Akash Joshi <akash.joshi@viit.ac.in>
2020-07-26 15:16:23 +0530

    Add Profile page

--- pages/profile.js
+++ pages/profile.js
@@ -11,8 +11,6 @@ export default function Home() {
       <main>
         <h1 className="title">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-          <br />
-          <a href="/profile">Go to Profile</a>
         </h1>
 
         <p className="description">
